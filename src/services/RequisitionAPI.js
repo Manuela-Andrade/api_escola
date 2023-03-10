edbbf6a786b79459922b60358ef58d5bbbde29db
manuandrade-crf <rjmanuelaandrade@gmail.com>
2023-03-10 20:01:08 -0300

    Iniciando repositório

--- src/services/RequisitionAPI.js
+++ src/services/RequisitionAPI.js
@@ -3,17 +3,17 @@ import axios from "axios"
 export class AxiosApi {
 
    static async Get(path) {
-        return await axios.get(`https://agapt.onrender.com${path}`)
+        return await axios.get(`https://https-escola-onrender-com.onrender.com${path}`)
     }
     static async Post(path, body) {
-        return await axios.post(`https://agapt.onrender.com${path}`,
+        return await axios.post(`https://https-escola-onrender-com.onrender.com${path}`,
         body)
     }
     static async Put(path, body) {
-        return await axios.put(`https://agapt.onrender.com${path}`,
+        return await axios.put(`https://https-escola-onrender-com.onrender.com${path}`,
         body)
     }
     static async Delete(path) {
-        return await axios.delete(`https://agapt.onrender.com${path}`)
+        return await axios.delete(`https://https-escola-onrender-com.onrender.com${path}`)
     }
 }
